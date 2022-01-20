002f003b174b69397a23f00ff2006f639c2fdeed
Kravchuk Anton <Kravch-anton@mail.ru>
2022-01-20 23:05:28 +0300

    first commit

---
+++ src/nav/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from '../Header/Header.module.css'
+
+function Header() {
+    return (
+        <div className={styles.header}>
+            <h1>fdwgerg</h1>
+
+        </div>
+    );
+}
+
+export default Header;
