2607ffc909863b56e3429901fffc058806a3ce49
Kravchuk Anton <Kravch-anton@mail.ru>
2022-01-24 23:46:04 +0300

    refactor

--- src/Skills/skill/Skill.js
+++ src/Skills/skill/Skill.js
@@ -4,7 +4,6 @@ import styles from '../skill/Skill.module.css'
 function Skill(props) {
     return (
         <div className={styles.skill}>
-            <div className={styles.icon}></div>
             <h3>{props.title}</h3>
             <span className={styles.descr}> {props.descr}</span>
         </div>
