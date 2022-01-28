149ba6f0a5edafe3909514169f6b5f18008b4e56
Kravchuk Anton <Kravch-anton@mail.ru>
2022-01-28 22:58:43 +0300

    layout header

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,14 +1,26 @@
 import React from "react";
 import styles from "../Main/Main.module.css";
 import styleContainer from '../common/styles/Container.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-function Main() {
+export const Main = () => {
     return (
         <div className={styles.main}>
             <div className={styleContainer.container}>
-                <div className={styles.text}><span>Hi There</span>
-                    <h1>I am Anton Kravchuk</h1></div>
-                <div className={styles.photo}><img src="../image/photo.jpeg" alt="#"/></div>
+                <div className={styles.wrapper}>
+                    <div className={styles.text}>
+                        <h1>Hi, I’m Anton Kravchuk</h1>
+                        <h2>Front-end developer</h2></div>
+                    <div className={styles.contacts}>
+                        <div className={styles.contactsText}>FIND WITH ME</div>
+                        <div className={styles.contactsIcons}><FontAwesomeIcon icon={['fab', 'fa-telegram']}/> </div>
+                    </div>
+                </div>
+
+
+                <div className={styles.photo}>
+                    <div className={styles.image}></div>
+                </div>
             </div>
 
         </div>
--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from '../nav/Nav.module.css'
+import styleContainer from "../common/styles/Container.module.css";
 
 function Nav() {
     return (
         <div className={styles.nav}>
-            <a href="">Main</a>
-            <a href="">Skills</a>
-            <a href="">Projects</a>
-            <a href="">Contacts</a>
+             <a href="">MAIN</a>
+                <a href="">SKILLS</a>
+                <a href="">PROJECTS</a>
+                <a href="">CONTACTS</a>
+
 
         </div>
     );
